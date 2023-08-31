2c6fcbfd60aea843509cfe6db6976d4d1e643106
aaarajuan <aaa.rajuan@gmail.com>
2023-08-31 17:57:40 +0600

    learned arrow function

---
+++ arrow.js
@@ -0,0 +1,29 @@
+// function declaration
+/* function add (a, b){
+    const result = a + b;
+    return result;
+}
+const sum = add(1, 2);
+console.log(sum); */
+
+
+function add (a, b){
+    return a + b;
+}
+
+// function expression
+const add2 = function(a, b) {
+    return a + b;
+};
+
+// arrow function
+const add3 = (a, b) => a + b;
+const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+const multiply = (num1, num2) => num1 * num2;
+
+const sum = add4(1, 2, 5, 3);
+console.log(sum);
+
+const mult = multiply(5, 3);
+console.log(mult);
+
