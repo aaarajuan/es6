39887d98dd6fb417b962a0a682c36d19344cf93a
aaarajuan <aaa.rajuan@gmail.com>
2023-08-31 17:27:58 +0600

    adding default parameter

---
+++ default.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    return result;
+}
+
+// const sum = add(1, 2);
+// const sum = add(5);
+// const sum = add();
+
+function fullName(first, last) {
+    const full = first + ' ' + last;
+    console.log(full);
+    return full;
+}
+
+function friends (numbs = []) {
+    
+}
